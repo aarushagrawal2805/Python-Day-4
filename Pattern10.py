ff7f11b17b9c0dfa4c9a63ba0f09f6ce5e1af16e
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-11 16:06:09 +0530

    Pattern 10

---
+++ Pattern10.py
@@ -0,0 +1,28 @@
+'''
+*     *
+**   **
+*** ***
+*******
+*** ***
+**   **
+*     *
+
+'''
+
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print("*", end="")
+    for l in range(6-i):
+        print("  ",end="")
+    for m in range(1,i+1):
+        print("*",end="")
+    print()
+
+for i in range(6, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end="")
+    for j in range(6-i):
+        print("  ",end="")
+    for m in range(i):
+        print("*",end="")
+    print()
