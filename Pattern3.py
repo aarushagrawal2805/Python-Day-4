b11cc80689eab5419a10e53d5deab84f7bd32706
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-11 13:34:53 +0530

    Pattern 3

---
+++ Pattern3.py
@@ -0,0 +1,21 @@
+# Print the following pattern
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
+
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print("")
+for i in range(4, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print("")
