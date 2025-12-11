e828e52a276d7632c1ce45545d5c0c806075d1be
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-11 14:39:43 +0530

    Pattern7

---
+++ Pattern7.py
@@ -0,0 +1,14 @@
+'''
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+'''
+
+for i in range(1, 6):
+    for s in range(6 - i):
+        print(" ", end="")
+    for j in range(i):
+        print("* ", end="")
+    print()
