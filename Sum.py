38c11c9ad44e2ca68ac4c1fd66353143e4cfb3a6
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-11 12:26:21 +0530

    Sum of Numbers

--- Sum.py
+++ Sum.py
@@ -1,9 +1,13 @@
-#Calculate sum of all numbers from 1 to a given number
-i=0
+# Calculate sum of all numbers from 1 to a given number
 sum = 0
-s=int(input("Enter a number:"))
-while(i<=s):
-    sum+=i
-    i+=1
-print("Sum of Number",s,"is :",sum)
+s = int(input("Enter a number:"))
+# Using while loop
+'''while i <= s:
+    sum += i
+    i += 1
+print("Sum of Number", s, "is :", sum)'''
 
+# using for loop
+for i in range(0, s + 1):
+    sum += i
+print("Sum of Number", s, "is :", sum)
